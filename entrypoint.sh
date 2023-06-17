9e2810314673e664a58bf2ba353cab71be600741
VladimirKalita <105916007+VladimirKalita@users.noreply.github.com>
2023-06-17 22:41:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ devprofiler -- github /github/workspace "${GITHUB_REPOSITORY}"
 timestamp=$(date +%s)
 filename="${timestamp}-devprofile.jsonl.gz"
 mv devprofile.jsonl.gz "${filename}"
+dpkg -I /tmp/devprofiler.deb | grep Version
 
 curl -F "file=@${filename}"  https://gcscruncsql-k7jns52mtq-el.a.run.app/upload
